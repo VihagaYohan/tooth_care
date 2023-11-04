8599a76e72dd4ff967a21a7a292803d7fd640aa4
Vihaga Yohan <35211294+VihagaYohan@users.noreply.github.com>
2023-11-04 10:38:43 +0530

    Added - basic code layout for app text component. not completed

---
+++ app/components/app_text.tsx
@@ -0,0 +1,10 @@
+import React, {Component} from 'react';
+import {StyleSheet, Text} from 'react-native';
+
+const AppText = () => {
+  return <Text></Text>;
+};
+
+const styles = StyleSheet.create({});
+
+export default AppText;
