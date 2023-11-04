b69078e3e4a03062e3345e9e219c9101f810cb0d
Vihaga Yohan <35211294+VihagaYohan@users.noreply.github.com>
2023-11-04 10:28:23 +0530

    Removed - unecessary codes from app.tsx file

--- App.tsx
+++ App.tsx
@@ -7,15 +7,6 @@
 
 import React from 'react';
 import type {PropsWithChildren} from 'react';
-import {
-  SafeAreaView,
-  ScrollView,
-  StatusBar,
-  StyleSheet,
-  Text,
-  useColorScheme,
-  View,
-} from 'react-native';
 
 // navigation
 import {NavigationContainer} from '@react-navigation/native';
