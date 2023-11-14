c3939a7b473b9f25acf08fc52332a4e9149a6d20
Vihaga Yohan <35211294+VihagaYohan@users.noreply.github.com>
2023-11-14 16:59:29 +0530

    Added - UI layout for new patient

---
+++ app/data/gender.ts
@@ -0,0 +1,14 @@
+import {Gender} from '../domain/enums/Enum';
+
+const Genders = [
+  {
+    id: 1,
+    gender: Gender.Male,
+  },
+  {
+    id: 2,
+    gender: Gender.Feamel,
+  },
+];
+
+export default Genders;
--- app/screens/patients/NewPatients.tsx
+++ app/screens/patients/NewPatients.tsx
@@ -1,17 +1,133 @@
-import React, {Component} from 'react';
-import {StyleSheet, View} from 'react-native';
+import React, {Component, useState, useEffect} from 'react';
+import {StyleSheet, View, ScrollView} from 'react-native';
+import {Dropdown} from 'react-native-element-dropdown';
 
 // components
-import {UIContainer, UITextView} from '../../components';
+import {UIContainer, UITextInput, UITextView, UIButton} from '../../components';
+
+// constants
+import {COLORS, STYLES} from '../../constants';
+
+// pre-defined data
+import Genders from '../../data/gender';
 
 const NewPatient = () => {
+  const [isFocus, setIsFocus] = useState(false);
+  const [value, setValue] = useState(null);
+  const [firstName, setFirstName] = useState<string>();
+  const [lastName, setLastName] = useState<string>();
+  const [address, setAddress] = useState<string>();
+  const [telephone, setTelephone] = useState<string>();
+  const [age, setAge] = useState<string>();
+  const [gender, setGender] = useState();
+
   return (
     <UIContainer>
-      <UITextView text="New patient screen" />
+      <ScrollView showsHorizontalScrollIndicator={false}>
+        <View style={[styles.row, STYLES.flexRow]}>
+          <UITextInput
+            placeholder="First name"
+            placeholderTextColor={COLORS.white}
+            onChangeText={value => setFirstName(value)}
+          />
+          <UITextInput
+            placeholder="Last name"
+            placeholderTextColor={COLORS.white}
+            onChangeText={value => setLastName(value)}
+          />
+        </View>
+
+        <UITextInput
+          placeholder="Address"
+          placeholderTextColor={COLORS.white}
+          onChangeText={value => setAddress(value)}
+        />
+        <UITextInput
+          placeholder="Telephone"
+          placeholderTextColor={COLORS.white}
+          onChangeText={value => setTelephone(value)}
+        />
+        <UITextInput
+          placeholder="Age"
+          placeholderTextColor={COLORS.white}
+          onChangeText={value => setAge(value)}
+        />
+
+        <Dropdown
+          style={[
+            styles.dropdown,
+            isFocus && {borderColor: COLORS.blue.blue800},
+          ]}
+          placeholderStyle={styles.placeholderStyle}
+          selectedTextStyle={styles.selectedTextStyle}
+          inputSearchStyle={styles.inputSearchStyle}
+          iconStyle={styles.iconStyle}
+          data={Genders}
+          search
+          maxHeight={300}
+          labelField="gender"
+          valueField="id"
+          placeholder={!isFocus ? 'Select gender' : '...'}
+          searchPlaceholder="Search..."
+          value={value}
+          onFocus={() => setIsFocus(true)}
+          onBlur={() => setIsFocus(false)}
+          onChange={(item: any) => {
+            delete item._index;
+            setGender(item);
+            setIsFocus(false);
+          }}
+        />
+
+        <UIButton label="Add Patient" onClick={() => console.log(firstName)} />
+      </ScrollView>
     </UIContainer>
   );
 };
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  row: {
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  container: {
+    backgroundColor: 'white',
+    padding: 16,
+  },
+  dropdown: {
+    height: 50,
+    borderColor: 'gray',
+    borderWidth: 0.5,
+    borderRadius: 8,
+    paddingHorizontal: 8,
+    marginBottom: 10,
+  },
+  icon: {
+    marginRight: 5,
+  },
+  label: {
+    position: 'absolute',
+    backgroundColor: 'white',
+    left: 22,
+    top: 8,
+    zIndex: 999,
+    paddingHorizontal: 8,
+    fontSize: 14,
+  },
+  placeholderStyle: {
+    fontSize: 16,
+  },
+  selectedTextStyle: {
+    fontSize: 16,
+  },
+  iconStyle: {
+    width: 20,
+    height: 20,
+  },
+  inputSearchStyle: {
+    height: 40,
+    fontSize: 16,
+  },
+});
 
 export default NewPatient;
