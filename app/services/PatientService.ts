0559d2e4f73628ebd51b3cb3bbff0eb9d49f11f3
Vihaga Yohan <35211294+VihagaYohan@users.noreply.github.com>
2023-11-14 18:05:26 +0530

    Added - Patient service

---
+++ app/services/PatientService.ts
@@ -0,0 +1,15 @@
+import {Patient} from '../domain/entities';
+import {CollectionTypes} from '../domain/enums/Enum';
+import Store from '../globalStore/Store';
+
+// get all patients
+export const getAllPatients = () => {
+  let store = new Store<Patient>();
+  store.getList(CollectionTypes.Patient);
+};
+
+// add new patient
+export const addNewPatient = (item: Patient) => {
+  let store = new Store<Patient>();
+  store.addItemToCollection(item);
+};
